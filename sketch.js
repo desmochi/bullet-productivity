b995be0372e7191d4dfd332a33a53122c0ee2a0e
syoung212 <57546744+syoung212@users.noreply.github.com>
2021-08-04 13:17:14 -0400

    sketch edited

--- sketch.js
+++ sketch.js
@@ -9,7 +9,6 @@ var s = function (sketch){ //wrapper function creates the whole new p5 thing
         sketch.createCanvas(550, 550);
         sketch.colorMode(sketch.HSB, 360, 100, 100);
         sketch.backgroundColor = 95;
-        //c.position(0,0);
         sketch.createCanvas(550, 550).style('pointer-events', 'none'); 
         // a particular graphic element is not the target of pointer events
         sketch.clear();
