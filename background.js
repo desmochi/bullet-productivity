b5e0dc61b539580691772585ee19a5672a919371
desmochi <dcchi@live.com>
2021-08-04 08:50:39 -0800

    created popup and buttons

--- background.js
+++ background.js
@@ -2,22 +2,13 @@ chrome.contextMenus.create ({
     "title": "Pomodoro Productivity",
     id: "test",
     "contexts": ["selection"],
-    /*"onclick": openTab()*/
 });
-
-//function openTab(){
-//    return function(info, tab){
-//      let text = info.selectionText;
-//      let indexfile = index.html; 
-//      chrome.tabs.create ({index: tab.index + 1, url: indexfile, selected: true});
-//    }
-//};
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if(info.menuItemId = "test") {
         return function(info, tab) {
             let text = info.selectionText;
-            let indexfile = index.html; 
+            let indexfile = popup.html; 
             chrome.tabs.create ({index: tab.index + 1, url: indexfile, selected: true});
         }  
     }
---
+++ pomodoro.js
@@ -0,0 +1,19 @@
+// Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
+// Add to this list as you consult the p5.js documentation for other functions.
+/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
+          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+          mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
+          keyCode, push, pop, drawSprites, httpGet, keyIsDown, max, min, textFont, textAlign, CENTER, UP_ARROW, 
+          sqrt, noFill, collideRectRect, LEFT_ARROW, frameRate, RIGHT_ARROW, DOWN_ARROW, textSize, round, mouseClicked, 
+          keyPressed */
+
+function setup() {
+    //Creates canvas
+    createCanvas(550, 550);
+    colorMode(HSB, 360, 100, 100);
+    backgroundColor = 95;
+}
+
+function draw() {
+
+}
